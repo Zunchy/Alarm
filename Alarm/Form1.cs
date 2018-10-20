790b1828c6be4bb5f2b2f15d693fb6f65ce63fd4
JayKrisp <ragected@gmail.com>
2018-10-20 10:11:55 -0400

    tryna get the working number

--- Alarm/Form1.cs
+++ Alarm/Form1.cs
@@ -9,6 +9,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using TextmagicRest;
+using TextmagicRest.Model;
+
 namespace Alarm
 {
     public partial class Form1 : Form
@@ -22,18 +25,29 @@ namespace Alarm
         {
             currentTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
 
-            var message = new MailMessage();
-            message.From = new MailAddress("ragected@gmail.com");
+            //    var message = new MailMessage();
+            //    message.From = new MailAddress("ragected@gmail.com");
 
-            message.To.Add(new MailAddress("2163189822@vtext.com"));//See carrier destinations below
-                                                                    //message.To.Add(new MailAddress("5551234568@txt.att.net"));
+            //    message.To.Add(new MailAddress("2167744556@vmobl.com"));//See carrier destinations below
+            //                                                            //message.To.Add(new MailAddress("5551234568@txt.att.net"));
 
-            //message.CC.Add(new MailAddress("carboncopy@foo.bar.com"));
-            message.Subject = "This is my subject";
-            message.Body = "This is the content";
+            //    //message.CC.Add(new MailAddress("carboncopy@foo.bar.com"));
+            //    message.Subject = "This is my subject";
+            //    message.Body = "This is the content";
 
-            var client = new SmtpClient();
-            client.Send(message);
+            //    var client = new SmtpClient("google.com");
+            //    client.Send(message);
+
+            var client = new Client("test", "my-api-key");
+            var link = client.SendMessage("Hello from TextMagic API", "+12167744556");
+            if (link.Success)
+            {
+                Console.WriteLine("Message ID {0} has been successfully sent", link.Id);
+            }
+            else
+            {
+                Console.WriteLine("Message was not sent due to following exception: {0}", link.ClientException.Message);
+          }
         }
 
 
