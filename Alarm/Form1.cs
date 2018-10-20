9e1a5b8253b384e9861c32f6cd1e80a80250ebd1
JayKrisp <ragected@gmail.com>
2018-10-20 02:18:21 -0400

    do not open PLEASE

--- Alarm/Form1.cs
+++ Alarm/Form1.cs
@@ -17,6 +17,42 @@ namespace Alarm
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
 
+
+
+        private void currentTimer_Tick(object sender, EventArgs e)
+        {
+            currentTime.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        private void alarmTimer_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void setAlarm_Click(object sender, EventArgs e)
+        {
+
+            lblAlarmTime.Text = alarmBox.Text;
+
+            if (amRadio.Checked)
+                lblAlarmTime.Text += " AM";
+            else
+                lblAlarmTime.Text += " PM";
+
+           
+            
+        }
+
+        private void timerCheck_Tick(object sender, EventArgs e)
+        {
+     
+            if (lblAlarmTime.Text == currentTime.Text)
+                MessageBox.Show("Yo dawg wake yo stanky ass up");
+        }
     }
 }
