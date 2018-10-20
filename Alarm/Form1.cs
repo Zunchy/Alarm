a5b44e2629ab500b3f363bafe5edd9b9c875eaf4
Zunchy <zwaterbury1289@gmail.com>
2018-10-20 12:48:58 -0400

    Stop Music When Off Button Clicked.

--- Alarm/Form1.cs
+++ Alarm/Form1.cs
@@ -79,6 +79,13 @@ namespace Alarm
             {
                 audio.Play();
 
+                Form2 form2 = new Form2(audio);
+                form2.Show();
+
+  
+
+                this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+
             }
         }
 
@@ -110,7 +117,7 @@ namespace Alarm
                 
 
            
-              // Console.WriteLine(message.Sid);
+              
             }
 
         private void amRadio_CheckedChanged(object sender, EventArgs e)
